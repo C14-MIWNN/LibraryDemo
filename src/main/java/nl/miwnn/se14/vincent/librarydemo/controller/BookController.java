586c6732c14c9222802904a31190376e638bacaa
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-10-21 15:21:27 +0200

    Book Form and delete link

--- src/main/java/nl/miwnn/se14/vincent/librarydemo/controller/BookController.java
+++ src/main/java/nl/miwnn/se14/vincent/librarydemo/controller/BookController.java
@@ -1,9 +1,14 @@
 package nl.miwnn.se14.vincent.librarydemo.controller;
 
+import nl.miwnn.se14.vincent.librarydemo.model.Book;
 import nl.miwnn.se14.vincent.librarydemo.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Vincent Velthuizen
@@ -17,10 +22,34 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/book/overview")
+    @GetMapping({"/", "/book/overview"})
     private String showBookOverview(Model datamodel) {
         datamodel.addAttribute("allBooks", bookRepository.findAll());
 
         return "bookOverview";
+    }
+
+    @GetMapping("/book/new")
+    private String showBookForm(Model datamodel) {
+        datamodel.addAttribute("newBook", new Book());
+
+        return "bookForm";
+    }
+
+    @PostMapping("/book/new")
+    private String saveOrUpdateBook(@ModelAttribute("newBook") Book bookToBeSaved, BindingResult result) {
+        if (result.hasErrors()) {
+            System.err.println(result.getAllErrors());
+            return "redirect:/book/overview";
+        }
+
+        bookRepository.save(bookToBeSaved);
+        return "redirect:/book/overview";
+    }
+
+    @GetMapping("/book/delete/{bookId}")
+    private String deleteBook(@PathVariable("bookId") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/book/overview";
     }
 }
