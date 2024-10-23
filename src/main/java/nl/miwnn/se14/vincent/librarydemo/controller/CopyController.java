1e144a80e83908f9032914cfbc3c0d1f75c8d2fe
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-10-23 09:54:17 +0200

    Added the concept of a copy and a way to add them

---
+++ src/main/java/nl/miwnn/se14/vincent/librarydemo/controller/CopyController.java
@@ -0,0 +1,43 @@
+package nl.miwnn.se14.vincent.librarydemo.controller;
+
+import nl.miwnn.se14.vincent.librarydemo.model.Book;
+import nl.miwnn.se14.vincent.librarydemo.model.Copy;
+import nl.miwnn.se14.vincent.librarydemo.repositories.BookRepository;
+import nl.miwnn.se14.vincent.librarydemo.repositories.CopyRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+/**
+ * @author Vincent Velthuizen
+ * Handle all requests related primarily to copies
+ */
+
+@Controller
+public class CopyController {
+    private final BookRepository bookRepository;
+    private final CopyRepository copyRepository;
+
+    public CopyController(BookRepository bookRepository, CopyRepository copyRepository) {
+        this.bookRepository = bookRepository;
+        this.copyRepository = copyRepository;
+    }
+
+    @GetMapping("/copy/new/{bookId}")
+    private String createNewCopy(@PathVariable("bookId") Long bookId) {
+        Optional<Book> bookOptional = bookRepository.findById(bookId);
+
+        if (bookOptional.isEmpty()) {
+            System.err.printf("Could not retrieve book with ID: %d\n", bookId);
+            return "redirect:/";
+        }
+
+        Copy copy = new Copy();
+        copy.setBook(bookOptional.get());
+        copyRepository.save(copy);
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/nl/miwnn/se14/vincent/librarydemo/model/Copy.java
@@ -0,0 +1,50 @@
+package nl.miwnn.se14.vincent.librarydemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+/**
+ * @author Vincent Velthuizen
+ * A physical copy of a book that someone might borrow from our library
+ */
+
+@Entity
+public class Copy {
+    @Id @GeneratedValue
+    private Long copyId;
+
+    private Boolean available;
+
+    @ManyToOne
+    private Book book;
+
+    public Copy() {
+        available = true;
+    }
+
+    public Long getCopyId() {
+        return copyId;
+    }
+
+    public void setCopyId(Long copyId) {
+        this.copyId = copyId;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
